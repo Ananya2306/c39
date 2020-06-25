3b423f7fcf43f55059efe0a466d355d0855c6a95
Ananya2306 <amitarora_1@yahoo.com>
2020-06-25 09:08:17 +0530

    js/Form.js

--- js/Form.js
+++ js/Form.js
@@ -5,6 +5,7 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset = createButton('Reset');
   }
   hide(){
     this.greeting.hide();
@@ -19,7 +20,7 @@ class Form {
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-2
+    this.reset.position(displayWidth/2+ 500,displayHeight/2 - 300);
     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
@@ -32,5 +33,11 @@ class Form {
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
 
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
+    });
   }
+
+
 }
--- js/Game.js
+++ js/Game.js
@@ -70,6 +70,8 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
+          fill("red")
+          ellipse(x,y,100,100);
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
